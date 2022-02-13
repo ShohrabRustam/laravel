6cf99d047ee64c53576405123a57849c4af1ddb0
Shohrab Rustam <mohdrustam001@gmail.com>
2022-02-13 16:17:30 +0530

    single action controller

---
+++ SingleActionController/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+
+
+@extends('Layout.main')
+@push('title')
+<title>About</title>
+@endpush
+@section('main-section')
+    <h1 class="text-center">About</h1>
+@endsection
---
+++ SingleActionController/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('Layout.main')
+@push('title')
+<title>Home</title>
+@endpush
+@section('main-section')
+    <h1 class="text-center">Home</h1>
+@endsection
+
